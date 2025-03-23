6f81335097eba89ae7df8b1e665cc16144373cad
Mariia Davydova <davydova.meri@gmail.com>
2025-03-23 20:31:58 +0200

    fixed retries location

--- cypress.config.js
+++ cypress.config.js
@@ -7,15 +7,15 @@ export default defineConfig({
   video: true,
   viewportWidth: 1280,
   viewportHeight: 800,
+  retries: {
+    runMode: 2,      
+    openMode: 2   
+  },
   e2e: {
     baseUrl: 'https://example.cypress.io', 
     supportFile: "cypress/support/e2e.js",
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     defaultCommandTimeout: 8000,
     pageLoadTimeout: 60000,
-    retries: {
-      runMode: 2,      
-      openMode: 2   
-    },
   },
 });
